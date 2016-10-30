1b216f197d97757e96d9650fec8d61a3e48a6adb
gzlin <gzlin@coremail.cn>
2016-10-31 00:40:27 +0800

    Helab:添加数据库初始化文件

---
+++ src/main/resources/helab_init.sql
@@ -0,0 +1,88 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.16-log : Database - helab
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`helab` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `helab`;
+
+/*Table structure for table `admin_info` */
+
+DROP TABLE IF EXISTS `admin_info`;
+
+CREATE TABLE `admin_info` (
+  `admin_id` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(255) DEFAULT NULL,
+  `admin_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `member_info` */
+
+DROP TABLE IF EXISTS `member_info`;
+
+CREATE TABLE `member_info` (
+  `mem_id` bigint(20) NOT NULL,
+  `cn_name` varchar(255) DEFAULT NULL,
+  `en_name` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`mem_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `publication` */
+
+DROP TABLE IF EXISTS `publication`;
+
+CREATE TABLE `publication` (
+  `pub_id` bigint(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `desc` varchar(4096) DEFAULT NULL,
+  `snapshot` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`pub_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `research` */
+
+DROP TABLE IF EXISTS `research`;
+
+CREATE TABLE `research` (
+  `res_id` bigint(20) NOT NULL,
+  `cn_title` varchar(255) DEFAULT NULL,
+  `en_title` varchar(255) DEFAULT NULL,
+  `cn_desc` varchar(4096) DEFAULT NULL,
+  `en_desc` varchar(4096) DEFAULT NULL,
+  PRIMARY KEY (`res_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `seminar` */
+
+DROP TABLE IF EXISTS `seminar`;
+
+CREATE TABLE `seminar` (
+  `sem_id` bigint(20) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `speaker` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `sem_time` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`sem_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
